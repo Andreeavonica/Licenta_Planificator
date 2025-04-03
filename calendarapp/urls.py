85d2d6fae940ed2b41271f21c4814844ccc83544
andre <andreea.vonica03@e-uvt.ro>
2025-04-03 14:59:04 +0300

    added orar confirmat

--- calendarapp/urls.py
+++ calendarapp/urls.py
@@ -42,6 +42,8 @@ urlpatterns = [
     path("orar/", other_views.schedule_page, name="orar"),
     path("run-schedule/", other_views.run_schedule, name="run_schedule"),
     path("confirm-schedule/", other_views.confirm_schedule, name="confirm_schedule"),
+    path("orar-confirmat/", other_views.confirmed_schedule_view, name="confirmed-schedule"),
+
 
 
 ]
