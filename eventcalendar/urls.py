0873a3bf8e33d8ac6add4ebdd5e453f40fc58c29
andre <andreea.vonica03@e-uvt.ro>
2025-05-18 03:00:27 +0300

    static daphne

--- eventcalendar/urls.py
+++ eventcalendar/urls.py
@@ -19,9 +19,14 @@ from django.urls import path, include
 from .views import DashboardView
 
 
+from django.conf import settings
+from django.conf.urls.static import static
+import os
+
 urlpatterns = [
     path("", DashboardView.as_view(), name="dashboard"),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("", include("calendarapp.urls")),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'static'))
